e6192fca94f3758f47682c177d425489112b1ee8
Tom Lendy <tlendy25@gmail.com>
2018-02-19 21:42:07 -0600

    Corrected example.js

--- src/example.js
+++ src/example.js
@@ -1,9 +1,9 @@
 const Nightmare = require("nightmare")
-const nightmare = Nightmare({show: true})
+const nightmare = Nightmare({ show: true })
 
 nightmare
   .goto("https:/duckduckgo.com")
-  .type("search_form_input_homepage" , "github nightmare")
+  .type("#search_form_input_homepage" , "AR15.com")
   .click("#search_button_homepage")
   .wait("#r1-0 a.result__a")
   .evaluate(() => document.querySelector("#r1.0 a.result__a").href)
@@ -12,4 +12,3 @@ nightmare
   .catch((error) => {
     console.error("Search failed", error)
   })
-  
